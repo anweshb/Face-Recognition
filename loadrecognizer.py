1b0a62c3c099527d5e753986b754204b68f2b240
Anwesh Badapanda <anweshbadapanda@yahoo.in>
2020-03-23 00:31:14 +0530

    Added logging feature

--- loadrecognizer.py
+++ loadrecognizer.py
@@ -1,17 +1,17 @@
 import cv2
 import numpy as np
+from datetime import datetime
 
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 recognizer.read('E:\\Conda Projects\FD\\trainer\\trainer.yml')
 cascadePath = "E:\\Conda Projects\\FD\\haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascadePath)
 
-
 cam = cv2.VideoCapture(0)
 font = cv2.FONT_HERSHEY_SIMPLEX
 while True:
-    ret, im =cam.read()
-    gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+    ret, im = cam.read()
+    gray=cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     faces=faceCascade.detectMultiScale(gray, 1.2,5)
     for(x,y,w,h) in faces:
         cv2.rectangle(im,(x,y),(x+w,y+h),(225,0,0),2)
@@ -19,10 +19,13 @@ while True:
         if(conf<50):
             if(Id==1):
                 Id="Anwesh"
+                name = Id
         else:
             Id="Unknown"
+            name = Id
         cv2.putText(im,str(Id), (x,y+h),font, 1,(255,0,0))
-    cv2.imshow('im',im) 
+    cv2.imshow('im',im)
+    cv2.imwrite("E:\\Conda Projects\\FD\\logs."+name +' '+ str(datetime.now()) + ".jpg", gray[y:y+h,x:x+w]) 
     if cv2.waitKey(10) & 0xFF==ord('q'):
         break
 cam.release()
